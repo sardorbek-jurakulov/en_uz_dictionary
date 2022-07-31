db2fb1cb135a95dbfb796fed6110ebff19291dd5
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-07-31 19:19:04 +0500

    WIP

--- lib/word_details_page.dart
+++ lib/word_details_page.dart
@@ -5,9 +5,10 @@ class WordDetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // final args = ModalRoute.of(context)!.settings.arguments as Map;
-    List<Map> args = [];
-    args.add(ModalRoute.of(context)!.settings.arguments as Map<String, String>);
+    final args = ModalRoute.of(context)!.settings.arguments as Map;
+    // List<Map> args = [];
+    // args.add(ModalRoute.of(context)!.settings.arguments as Map<String, String>);
+    // List<Word> wordDetails = [];
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(),
@@ -16,9 +17,9 @@ class WordDetailsPage extends StatelessWidget {
           itemBuilder: (BuildContext context, int index) {
             return Column(
               children: [
-                Text(args[index]["wordInUzbek"]),
-                Text(args[index]["wordInEnglish"]),
-                Text(args[index]["wordDifinitionInUzbek"]),
+                Text(args["wordInUzbek"]),
+                Text(args["wordInEnglish"]),
+                Text(args["wordDifinitionInUzbek"]),
               ],
             );
           },
--- lib/words_list_page.dart
+++ lib/words_list_page.dart
@@ -5,8 +5,35 @@ class WordsListPage extends StatelessWidget {
   WordsListPage({Key? key}) : super(key: key);
 
   List<Word> words = [
-    Word("avans", "",
+    Word("avans", "", "maqsadga muvofiq ravishda oldinga siljish."),
+    Word("agent", "agent",
+        "Boshqa shaxs yoki guruh nomidan ish yurituvchi shaxs."),
+    Word("agentlik operatsiyalari", "agent operations",
+        "agentliklar aro bo'ladigan shartnomalar, bitimlar, kelishuvlar va boshqa holatlar."),
+    Word("agrar munosabatlar", "agrarian relations",
+        "agrar sohada kuzatiladigan turli hil munosabatlar, yig'ilishlar, kelishuvlar, mahorat darslari."),
+    Word("agrobiznes", "agrobusiness",
+        "agrobiznes yoki omonat banklarining biror shaxs yoki tashkilotga ma'lum miqdordagi mablag'ni berish haqida boshqa bank yoki kassaga yuborgan buyruq qog'ozi, masalan, jamg'armalar bankiga qo'yilgan pulni istalgan omonat bankidan olish uchun berilgan hujjat."),
+    Word("agrofirma", "agrofirm",
+        "agrofirma yoki omonat banklarining biror shaxs yoki tashkilotga ma'lum miqdordagi mablag'ni berish haqida boshqa bank yoki kassaga yuborgan buyruq qog'ozi, masalan, jamg'armalar bankiga qo'yilgan pulni istalgan omonat bankidan olish uchun berilgan hujjat."),
+    Word("agrosanoat birlashmasi", "agroindustry associations",
+        "agrosanoat bisrlashmasi yoki omonat banklarining biror shaxs yoki tashkilotga ma'lum miqdordagi mablag'ni berish haqida boshqa bank yoki kassaga yuborgan buyruq qog'ozi, masalan, jamg'armalar bankiga qo'yilgan pulni istalgan omonat bankidan olish uchun berilgan hujjat."),
+    Word("aholi daromadlari", "population incomes",
+        "aholi daromadlari yoki omonat banklarining biror shaxs yoki tashkilotga ma'lum miqdordagi mablag'ni berish haqida boshqa bank yoki kassaga yuborgan buyruq qog'ozi, masalan, jamg'armalar bankiga qo'yilgan pulni istalgan omonat bankidan olish uchun berilgan hujjat."),
+    Word("akkreditiv", "letter of credit",
         "bank yoki omonat banklarining biror shaxs yoki tashkilotga ma'lum miqdordagi mablag'ni berish haqida boshqa bank yoki kassaga yuborgan buyruq qog'ozi, masalan, jamg'armalar bankiga qo'yilgan pulni istalgan omonat bankidan olish uchun berilgan hujjat."),
+    Word("amaki", "uncle",
+        "amaki yoki omonat banklarining biror shaxs yoki tashkilotga ma'lum miqdordagi mablag'ni berish haqida boshqa bank yoki kassaga yuborgan buyruq qog'ozi, masalan, jamg'armalar bankiga qo'yilgan pulni istalgan omonat bankidan olish uchun berilgan hujjat."),
+    Word("aqilli", "clever",
+        "aqilli yoki omonat banklarining biror shaxs yoki tashkilotga ma'lum miqdordagi mablag'ni berish haqida boshqa bank yoki kassaga yuborgan buyruq qog'ozi, masalan, jamg'armalar bankiga qo'yilgan pulni istalgan omonat bankidan olish uchun berilgan hujjat."),
+    Word("amaliyot dasturi", "internship program",
+        "amaliyot dasturi yoki omonat banklarining biror shaxs yoki tashkilotga ma'lum miqdordagi mablag'ni berish haqida boshqa bank yoki kassaga yuborgan buyruq qog'ozi, masalan, jamg'armalar bankiga qo'yilgan pulni istalgan omonat bankidan olish uchun berilgan hujjat."),
+    Word("ahmoq", "stupid",
+        "ahmoq yoki omonat banklarining biror shaxs yoki tashkilotga ma'lum miqdordagi mablag'ni berish haqida boshqa bank yoki kassaga yuborgan buyruq qog'ozi, masalan, jamg'armalar bankiga qo'yilgan pulni istalgan omonat bankidan olish uchun berilgan hujjat."),
+    Word("aldamoq", "cheat",
+        "aldamoq yoki omonat banklarining biror shaxs yoki tashkilotga ma'lum miqdordagi mablag'ni berish haqida boshqa bank yoki kassaga yuborgan buyruq qog'ozi, masalan, jamg'armalar bankiga qo'yilgan pulni istalgan omonat bankidan olish uchun berilgan hujjat."),
+    Word("bilimli", "knowledgeable",
+        "bilimli yoki omonat banklarining biror shaxs yoki tashkilotga ma'lum miqdordagi mablag'ni berish haqida boshqa bank yoki kassaga yuborgan buyruq qog'ozi, masalan, jamg'armalar bankiga qo'yilgan pulni istalgan omonat bankidan olish uchun berilgan hujjat."),
   ];
 
   @override
